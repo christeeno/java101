5fb2fec2b0ef09d6e8baf77d1b2a3d771788d54b
CT-temphub <christeenotelfin@gmail.com>
2025-09-15 21:39:36 +0530

    all about arrays

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 //        STRING
@@ -11,5 +12,18 @@ public class Main {
 //        System.out.println(name1.substring(0,2));
 //
 //        ARRAYS
+        int[] marks=new int[3];
+        marks[0]=12;
+        marks[1]=32;
+        marks[2]=11;
+        System.out.println(marks[0]);
+
+
+        System.out.println(marks.length);
+        Arrays.sort(marks);
+        int[] mark={12,3,4,50};
+        int[][] final_marks={{12,23,34},{11,22,33}};
+        System.out.println(final_marks[0][0]);
+
     }
 }
