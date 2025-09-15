964d785f483db32b747d77266bfc04c695b45344
CT-temphub <christeenotelfin@gmail.com>
2025-09-15 21:06:55 +0530

    concatinate a string

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        String name1="A";
+        String name2="N";
+        System.out.println(name1+name2);
+
+
+
 
 
         for (int i = 1; i <= 5; i++)
