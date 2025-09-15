606f32e1609b23b9abe5153c1ce1c58559ecdc0c
CT-temphub <christeenotelfin@gmail.com>
2025-09-15 21:25:36 +0530

    all about strings

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        String name1="A";
-        String name2="N";
-        System.out.println(name1+name2);
-
-
-
-
-
-        for (int i = 1; i <= 5; i++)
-            System.out.println("i = " + i);
+//        STRING
+//        System.out.println("Hello and welcome!");
+//        String name1="Ant";
+//        String name2="N";
+//        System.out.println(name1+name2);
+//        System.out.println(name1.length());
+//        System.out.println(name1.charAt(0));
+//        String name3=name1.replace("a","b");
+//        System.out.println(name1.substring(0,2));
+//
+//        ARRAYS
     }
 }
